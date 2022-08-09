3e20cfd60ce17639708eb547440bbe8b78612e66
lumiseven <lumiseven@yahoo.com>
2022-08-09 09:55:41 +0800

    prometheus grafana demo confirm 1

---
+++ monitor/README.md
@@ -0,0 +1,11 @@
+## springcloud monitor
+
+### 监控方案
+- prometheus + grafana
+- zipkin
+- skywalking
+
+### prometheus + grafana 搭建
+[docker-compose](prometheus/docker/docker-compose.yml)
+
+`grafana` 中可以使用 [micrometer](https://grafana.com/grafana/dashboards/4701-jvm-micrometer/) 模板
---
+++ monitor/prometheus/src/main/kotlin/code/lumiseven/demo/skt/stream/dubbo/monitor/prometheus/PrometheusApp.kt
@@ -0,0 +1,22 @@
+package code.lumiseven.demo.skt.stream.dubbo.monitor.prometheus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+
+@SpringBootApplication
+class PrometheusApp {
+    @Bean
+    fun configurer(@Value("\${spring.application.name}") applicationName: String):
+            MeterRegistryCustomizer<MeterRegistry> {
+        return MeterRegistryCustomizer<MeterRegistry>
+        { registry -> registry.config().commonTags("application", applicationName) }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<PrometheusApp>(*args)
+}
